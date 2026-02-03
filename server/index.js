7d334dade9fabb3254ce108c42a03606ca0389a7
Francisco <franksnake08@gmail.com>
2026-02-03 15:54:14 -0600

    Corregido la linea duplicada 2

--- server/index.js
+++ server/index.js
@@ -1248,7 +1248,7 @@ app.post("/api/email/enviar", verifyToken, async (req, res) => {
 });
 
 // --- RUTAS DE ADMIN ---
-const adminRouter = express.Router();
+// const adminRouter = express.Router();
 adminRouter.use(isAdmin); // ¡Importante! 'isAdmin' se aplica a todas las rutas de 'adminRouter'
 // ==============================================================
 // MÓDULO DE CORREO DINÁMICO (Multiusuario) - CON DEPURACIÓN
