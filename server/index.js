ff8cb5aabec2e0a61c49849cd6b192377b0d7e71
Francisco <franksnake08@gmail.com>
2026-02-25 16:16:43 -0600

    se restauro index para el correo en cpanel 11

--- server/index.js
+++ server/index.js
@@ -186,9 +186,17 @@ async function crearCorreoCpanel(usuario, passwordCorreo) {
     ).toString("base64");
 
     const response = await axios.get(`${url}?${params.toString()}`, {
-      headers: { Authorization: `Basic ${authString}` },
+      headers: {
+        Authorization: `Basic ${authString}`,
+        // Agregamos estos headers para simular un navegador real
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        Accept: "application/json, text/plain, */*",
+        "Accept-Language": "es-MX,es;q=0.9,en-US;q=0.8,en;q=0.7",
+        Connection: "keep-alive",
+      },
       validateStatus: function (status) {
-        return status >= 200 && status < 500; // Para capturar errores HTTP 400 o 401 en lugar de ir al catch
+        return status >= 200 && status < 500;
       },
     });
 
