7ab5a2406ca8588ecbea5e820d7dee76dca105fd
Francisco <franksnake08@gmail.com>
2026-02-25 15:25:26 -0600

    se restauro index para el correo en cpanel 7

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const MailComposer = require("nodemailer/lib/mail-composer");
-const nodemailer = require("nodemailer");
 
 const https = require("https");
 
