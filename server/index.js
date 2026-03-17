bceb8a1250b5abc8b12046c3baa6647b60d48f3f
Frank <franksnake08@gmail.com>
2026-03-17 10:51:51 -0600

    Se agrego una validacion para las calificaciones y no mande correos repeditos 2

--- server/index.js
+++ server/index.js
@@ -9575,7 +9575,6 @@ cron.schedule("1 0 1 * *", async () => {
   }
 });
 
-const PORT = process.env.PORT || 3001; // <-- ESTO ES VITAL PARA RENDER
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
